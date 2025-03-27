f3722a5706b41ae87b3e9e7c185972b2f104e81b
chickiexd <contact@chickiexd.com>
2025-03-27 21:43:13 +0100

    feat: migrate for recipes

---
+++ cmd/migrate/migrations/000003_add_recipe_functionality.down.sql
@@ -0,0 +1,5 @@
+DROP TABLE IF EXISTS recipe_has_ingredients;
+DROP TABLE IF EXISTS instructions;
+DROP TABLE IF EXISTS recipes;
+DROP TABLE IF EXISTS meal_types;
+
---
+++ cmd/migrate/migrations/000003_add_recipe_functionality.up.sql
@@ -0,0 +1,45 @@
+-- Create meal_types table
+CREATE TABLE meal_types (
+    meal_type_id SERIAL PRIMARY KEY,
+    name TEXT UNIQUE NOT NULL
+);
+
+-- Some default meal types
+INSERT INTO meal_types (name) VALUES
+  ('breakfast'),
+  ('main'),
+  ('appetizer'),
+  ('snack'),
+  ('dessert');
+
+-- Create recipes table
+CREATE TABLE recipes (
+    recipe_id     SERIAL PRIMARY KEY,
+    title         TEXT NOT NULL,
+    description   TEXT,
+    public        BOOLEAN DEFAULT FALSE,
+    cook_time     INTEGER,
+    servings      INTEGER,
+    image         TEXT,
+    meal_type_id  INTEGER REFERENCES meal_types(meal_type_id),
+    user_id       INTEGER NOT NULL REFERENCES users(user_id),
+    created_at    TIMESTAMP DEFAULT now(),
+    updated_at    TIMESTAMP DEFAULT now()
+);
+
+-- Create recipe_has_ingredients table (many-to-many + quantity)
+CREATE TABLE recipe_ingredients (
+    recipe_id      INTEGER NOT NULL REFERENCES recipes(recipe_id),
+    ingredient_id  INTEGER NOT NULL REFERENCES ingredients(ingredient_id),
+    quantity       DOUBLE PRECISION NOT NULL DEFAULT 1.0,
+    PRIMARY KEY (recipe_id, ingredient_id)
+);
+
+-- Create instructions table
+CREATE TABLE instructions (
+    instruction_id SERIAL PRIMARY KEY,
+    recipe_id      INTEGER NOT NULL REFERENCES recipes(recipe_id),
+    step_number    INTEGER NOT NULL,
+    description    TEXT NOT NULL
+);
+
