d8329bfe581dcf1302cbf71e836f3784dc12ca69
chickiexd <contact@chickiexd.com>
2025-06-05 14:59:40 +0200

    chore: temp disabled unused handler

--- internal/handler/users.go
+++ internal/handler/users.go
@@ -1,35 +1,35 @@
 package handler
 
-import (
-	"context"
-	"net/http"
-
-	"github.com/chickiexd/zenful_shopping/internal/service"
-	"github.com/chickiexd/zenful_shopping/internal/store"
-	"github.com/gin-gonic/gin"
-)
-
-type UserHandler interface {
-	Create(c *gin.Context)
-}
-
-type userHandler struct {
-	service service.Service
-}
-
-func (h *userHandler) Create(c *gin.Context) {
-	var user store.User
-	if err := c.ShouldBindJSON(&user); err != nil {
-		c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid request"})
-		return
-	}
-
-	err := h.service.Users.Create(context.Background(), &user)
-	if err != nil {
-		c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to create user"})
-		return
-	}
-
-	c.JSON(http.StatusCreated, gin.H{"message": "User created successfully", "user": user})
-}
-
+// import (
+// 	"context"
+// 	"net/http"
+//
+// 	"github.com/chickiexd/zenful_shopping/internal/service"
+// 	"github.com/chickiexd/zenful_shopping/internal/store"
+// 	"github.com/gin-gonic/gin"
+// )
+//
+// type UserHandler interface {
+// 	Create(c *gin.Context)
+// }
+//
+// type userHandler struct {
+// 	service service.Service
+// }
+//
+// func (h *userHandler) Create(c *gin.Context) {
+// 	var user store.User
+// 	if err := c.ShouldBindJSON(&user); err != nil {
+// 		c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid request"})
+// 		return
+// 	}
+//
+// 	err := h.service.Users.Create(context.Background(), &user)
+// 	if err != nil {
+// 		c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to create user"})
+// 		return
+// 	}
+//
+// 	c.JSON(http.StatusCreated, gin.H{"message": "User created successfully", "user": user})
+// }
+//
