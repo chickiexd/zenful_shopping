6daede90da9f973debdf230c563b0b9cf8698c00
chickiexd <contact@chickiexd.com>
2025-06-27 13:42:04 +0200

    fix: dont add pantry item if already exists

--- internal/service/pantry.go
+++ internal/service/pantry.go
@@ -4,6 +4,7 @@ import (
 	"github.com/chickiexd/zenful_shopping/internal/dto"
 	"github.com/chickiexd/zenful_shopping/internal/logger"
 	"github.com/chickiexd/zenful_shopping/internal/store"
+	"gorm.io/gorm"
 )
 
 type pantryService struct {
@@ -31,13 +32,21 @@ func (s *pantryService) Create(ingredient_id uint) error {
 	pantry_ingredient := &store.PantryIngredient{
 		IngredientID: ingredient_id,
 	}
-	_, err := s.storage.Pantry.Create(pantry_ingredient)
+	existing_entry, err := s.storage.Pantry.GetByIngredientID(ingredient_id)
+	if err != nil && err != gorm.ErrRecordNotFound {
+		logger.Log.Errorw("Error checking existing ingredient in pantry", "error", err, "ingredient_id", ingredient_id)
+		return err
+	}
+	if existing_entry != nil {
+		logger.Log.Warnw("Ingredient already exists in pantry", "ingredient_id", ingredient_id)
+		return nil
+	}
+	_, err = s.storage.Pantry.Create(pantry_ingredient)
 	if err != nil {
 		logger.Log.Errorw("Error adding ingredient to pantry", "error", err, "ingredient_id", ingredient_id)
 		return err
 	}
 	return nil
-
 }
 
 func (s *pantryService) Delete(ingredient_id uint) error {
